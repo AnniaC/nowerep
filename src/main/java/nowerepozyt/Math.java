1fd7afc210a07ab727ea37b52d5afc215ce41172
pWojslaw <p.wojslaw@gmail.com>
2017-01-05 19:52:29 +0100

    dodaje klasy

---
+++ src/main/java/nowerepozyt/Math.java
@@ -0,0 +1,15 @@
+package nowerepozyt;
+
+import java.util.List;
+
+/**
+ * Created by RENT on 2017-01-05.
+ */
+public class Math {
+    public static List<Factors> getFactors (int number ) {
+
+
+
+        return null;
+    }
+}
